586e71f54455d624c9c5b6569b60cfb91fa18cec
Suraj Saini <s2008saini@gmail.com>
2025-11-10 23:41:28 -0800

    10nov2025_first_commit

---
+++ blog/blog-script.js
@@ -0,0 +1,10 @@
+// Blog post navigation and interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize blog functionality
+    initializeBlog();
+});
+
+function initializeBlog() {
+    // Any future blog interactivity can be added here
+    console.log('Blog initialized');
+}
